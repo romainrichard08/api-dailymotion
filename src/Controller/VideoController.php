41bade1b6e5d688349284d882d14388614c872b1
romainrichard08 <romain.richard.08@hotmail.fr>
2018-08-06 22:41:27 +0200

    POST one video

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Video;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
 
 class VideoController extends Controller
 {
@@ -25,7 +27,20 @@ class VideoController extends Controller
 
         return $response;
     }
-}
 
+    /**
+     * @Route("/video", name="video_create")
+     * @Method({"POST"})
+     */
+    public function createAction(Request $request)
+    {
+        $data = $request->getContent();
+        $video = $this->get('jms_serializer')->deserialize($data, 'App\Entity\Video', 'json');
 
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($video);
+        $em->flush();
+
+        return new Response('', Response::HTTP_CREATED);
+    }
 }
